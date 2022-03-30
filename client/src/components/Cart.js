5d4acf2126870280759af0c3f02d5f4fe46e3049
baljot23 <92813636+baljot23@users.noreply.github.com>
2022-03-30 17:18:46 -0400

    CreateCart.js

---
+++ client/src/components/Cart.js
@@ -0,0 +1,12 @@
+const Cart = () => {
+  return (
+    <>
+      <div>Show items in cart</div>
+      <div>Delete item button</div>
+      <div>Modify item quantity import</div>
+      <div>Checkout button</div>
+    </>
+  );
+};
+
+export default Cart;
