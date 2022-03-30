fb606b41c8669357815d702edf00bc52173f82ef
baljot23 <92813636+baljot23@users.noreply.github.com>
2022-03-30 17:26:06 -0400

    CreateCheckoutPage.js

---
+++ client/src/components/Checkout.js
@@ -0,0 +1,12 @@
+const Checkout = () => {
+  return (
+    <>
+      <div>Order Detail</div>
+      <div>Form (name, address, email, credit card)</div>
+      <div>Back to cart button</div>
+      <div>Place order button</div>
+    </>
+  );
+};
+
+export default Checkout;
