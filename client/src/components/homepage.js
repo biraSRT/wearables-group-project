d85869e52ad8acc9dd96454994cb97c74d71f096
Veronika Vaisblay <v.vaisblay@gmail.com>
2022-03-30 16:42:28 -0400

    Created homepage component

---
+++ client/src/components/homepage.js
@@ -0,0 +1,7 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const homepage = () => {
+  return <div>hello</div>;
+};
