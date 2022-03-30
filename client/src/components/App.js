1aa343fd9f6308f0e6d44a15ad25be138ce33523
baljot23 <92813636+baljot23@users.noreply.github.com>
2022-03-30 16:34:18 -0400

    CreateApp.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,30 @@
-import { useState, useEffect } from "react";
-
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <GlobalStyles />
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route exact path="/item">
+            <Item />
+          </Route>
+          <Route exact path="/cart">
+            <Cart />
+          </Route>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+          <Route path="/confirmation">
+            <Confirmation />
+          </Route>
+          <Route path="">404: Oops!</Route>
+        </Switch>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
