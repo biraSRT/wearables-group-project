88fca1b6e187b5bda089277f61b6deff572df0d7
Noah Lee <lee.noah@outlook.com>
2022-03-30 17:01:10 -0400

    updated batchImport.js

--- server/batchImport.js
+++ server/batchImport.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const { MONGO_URI } = process.env;
 
 const items = require("./data/items.json");
+const companies = require("./data/companies.json");
 
 const options = {
   useNewUrlParser: true,
@@ -15,7 +16,8 @@ const batchImport = async () => {
   await client.connect();
   const db = client.db();
   try {
-    await db.collection("items").insertMany(items);
+    // await db.collection("items").insertMany(items);
+    await db.collection("companies").insertMany(companies);
     console.log("Import success");
   } catch (err) {
     console.log(err.message);
