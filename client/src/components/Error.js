ea403fd5afefd29180bec884f3f8c126c8c442ab
biraSRT <95512405+biraSRT@users.noreply.github.com>
2022-03-31 17:11:46 -0400

    styled Error component

--- client/src/components/Error.js
+++ client/src/components/Error.js
@@ -1,9 +1,66 @@
+import styled from  'styled-components';
+import { FaBomb } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 const Error = () => {
   return (
     <>
-      <div>Show error message</div>
+      <Wrapper>
+        <FaBomb size="5rem"/>
+        <h1>Oops! This is not what you are looking for.</h1>
+        <StyledLink to="/">Return To Homepage</StyledLink>
+      </Wrapper>
     </>
   );
 };
+
+
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 20vh;
+
+  @keyframes slideInFromLeft {
+    0% {
+      transform: translateX(-100%);
+    }
+    100% {
+      transform: translateX(0);
+    }
+  }
+
+  animation: 1s ease-out 0s 1 slideInFromLeft;
+  
+
+  h1{
+    margin-top: 20px;
+    background-color: #f5f5f5;
+    font-size: 24px;
+  }
+
+  margin-bottom: 20px;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: black;
+  margin-top: 20px;
+  border: 1px solid black;
+  padding: 10px;
+  font-size: 20px;
+  border-radius: 10px;
+  transition: background-color 0.2s ease-out 20ms;
+  transition: all .2s ease-in-out;
+
+  &:hover {
+    background-color: lightgray;
+    transform: scale(1.1);
+  }
+
+`;
+
 
 export default Error;
