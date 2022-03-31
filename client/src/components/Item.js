2ca6e70ba65c324f12ce88642b183de9e28e18fa
baljot23 <92813636+baljot23@users.noreply.github.com>
2022-03-30 21:32:40 -0400

    update Item.js

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -1,12 +1,62 @@
+import styled from "styled-components";
+
 const Item = () => {
   return (
     <>
-      <div>Item Image</div>
-      <div>Item Information</div>
-      <div>Amount Input</div>
-      <div>Add To Cart Button</div>
+      <Container>
+        <Image>Item Image</Image>
+        <Wrapper>
+          <div>Item Information</div>
+          <div>QTY:</div>
+          <Input type="number"></Input>
+
+          <CartButton>Add To Cart Button</CartButton>
+        </Wrapper>
+      </Container>
     </>
   );
 };
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-top: 10%;
+`;
+
+const Wrapper = styled.div`
+  border: 2px solid lavender;
+  margin: 50px;
+  width: 20%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  padding: 7% 0;
+`;
+
+const Image = styled.div`
+  border: 4px solid purple;
+  width: 5%;
+  padding: 150px;
+  margin: 150px;
+  margin-right: 0;
+`;
+
+const Input = styled.input`
+  border: 4px solid lightcyan;
+  width: 20%;
+  height: 20%;
+  text-align: center;
+`;
+
+const CartButton = styled.button`
+  border: 2px solid lightblue;
+  border-radius: 5%;
+  width: 35%;
+  height: 45px;
+  background-color: white;
+  cursor: pointer;
+`;
 
 export default Item;
