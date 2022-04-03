020bd1ff902dc49b34e2db77ea5d16f088066b16
Veronika Vaisblay <v.vaisblay@gmail.com>
2022-04-03 16:14:46 -0400

    about styling

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,10 +1,46 @@
 import styled from "styled-components";
-import Watches from "../watches.jpg";
+import TeamWork from "../team-work-dance.gif";
 
 const About = () => {
-  return <Image src={Watches} alt="watches image" />;
+  return (
+    <AboutContainer>
+      <Wrapper>
+        <Title>About BNTV</Title>
+        <AboutText>
+          BNTV specializes in all the latest gadgets on the market. Based in
+          Montreal, our organization came about at Concordia Bootcamps and we
+          are the best team!
+        </AboutText>
+        <Image src={TeamWork} alt="team work gif" />
+      </Wrapper>
+    </AboutContainer>
+  );
 };
 
-const Image = styled.img``;
+const AboutContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 50px;
+  font-family: Georgia, serif;
+`;
+
+const Wrapper = styled.div`
+  display: inline-block;
+  max-width: 50%;
+  text-align: center;
+`;
+const Title = styled.p`
+  font-size: 20px;
+  font-family: Georgia, serif;
+  text-align: center;
+  padding-bottom: 20px;
+  font-weight: bold;
+`;
+
+const AboutText = styled.p``;
+
+const Image = styled.img`
+  padding-top: 20px;
+`;
 
 export default About;
